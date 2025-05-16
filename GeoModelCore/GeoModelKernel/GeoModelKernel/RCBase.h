989a3535252ac4c77be0d89b55d2ee967b5f5076
Vakho Tsulaia <tsulaia@cern.ch>
2025-05-16 16:22:57 -0700

    athena_ci: changes in the simulation part of the pipeline
    - Added the detailed checks option to the default Run3 configuration
    - Introduced an SQLite-based Run3 configuration

--- GeoModelCore/GeoModelKernel/GeoModelKernel/RCBase.h
+++ GeoModelCore/GeoModelKernel/GeoModelKernel/RCBase.h
@@ -50,10 +50,7 @@ class RCBase {
     RCBase(const RCBase &right) = delete;
     RCBase & operator=(const RCBase &right) = delete;
     
-
     //  The reference count
     mutable std::atomic<unsigned> m_count{0};
-
 };
-
 #endif
