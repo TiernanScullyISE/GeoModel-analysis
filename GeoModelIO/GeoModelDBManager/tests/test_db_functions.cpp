ae4e4ea075d110059f7aba158a46eacc38926089
Shaun Roe <shaun.roe@cern.ch>
2025-06-12 20:32:00 +0000

    Enable additional tests

--- GeoModelIO/GeoModelDBManager/tests/test_db_functions.cpp
+++ GeoModelIO/GeoModelDBManager/tests/test_db_functions.cpp
@@ -1,8 +1,11 @@
 // GeoModel includes
 #include "GeoModelDBManager/GMDBManager.h"
 
+#include "gtest/gtest.h"
 #include <gtest/gtest.h>
 #include <filesystem>
+
+#define GTEST_COUT std::cerr << "[ MESSAGE  ] "
 
 class DatabaseTest : public ::testing::Test {
 protected:
@@ -37,28 +40,77 @@ protected:
     }
 };
 
-TEST_F(DatabaseTest, CreateDatabaseTest) {
-    // EXPECT_TRUE(dbManager->createDatabase());
+TEST_F(DatabaseTest, CanCreateDatabase) {
     EXPECT_TRUE(std::filesystem::exists(dbFile));
 }
 
-TEST_F(DatabaseTest, PopulateDatabaseTest) {
-    // ASSERT_TRUE(dbManager->createDatabase());
-    EXPECT_TRUE(dbManager->initDB());
+TEST_F(DatabaseTest, PropertiesOfAnUnintialisedDatabase) {
+  EXPECT_NO_THROW(dbManager->printAllDBTables());
+  EXPECT_TRUE(dbManager->checkIsDBOpen());
+  EXPECT_NO_THROW(dbManager->printAllPhysVols());
+  EXPECT_NO_THROW(dbManager->printAllFullPhysVols());
+  EXPECT_NO_THROW(dbManager->printAllLogVols());
+  EXPECT_NO_THROW(dbManager->printAllMaterials());
+  
 }
 
-// TEST_F(DatabaseTest, InsertDataTest) {
-//     ASSERT_TRUE(dbManager->createDatabase());
-//     ASSERT_TRUE(dbManager->initDB());
-
-//     EXPECT_TRUE(dbManager->insertData("key1", "value1"));
-// }
-
-// TEST_F(DatabaseTest, ReadDataTest) {
-//     ASSERT_TRUE(dbManager->createDatabase());
-//     ASSERT_TRUE(dbManager->populateDatabase());
-
-//     dbManager->insertData("key1", "value1");
-//     EXPECT_EQ(dbManager->readData("key1"), "value1");
-// }
+TEST_F(DatabaseTest, CanInitialiseDatabaseWithDefaultTables) {
+    EXPECT_TRUE(dbManager->initDB());
+    testing::internal::CaptureStdout();
+    dbManager->printAllDBTables();
+    std::string output = testing::internal::GetCapturedStdout();
+    //the following is a raw string literal; any change in formatting will change the actual comparison string!
+    const std::string expectedResponse{R"(AlignableTransforms
+ChildrenPositions
+Elements
+FullPhysVols
+FuncExprData
+Functions
+GeoNodesTypes
+IdentifierTags
+LogVols
+Materials
+Materials_Data
+NameTags
+PhysVols
+RootVolume
+SerialDenominators
+SerialIdentifiers
+SerialTransformers
+Shapes
+Shapes_Box
+Shapes_Cons
+Shapes_EllipticalTube
+Shapes_GenericTrap
+Shapes_GenericTrap_Data
+Shapes_Intersection
+Shapes_Para
+Shapes_Pcon
+Shapes_Pcon_Data
+Shapes_Pgon
+Shapes_Pgon_Data
+Shapes_Shift
+Shapes_SimplePolygonBrep
+Shapes_SimplePolygonBrep_Data
+Shapes_Subtraction
+Shapes_Torus
+Shapes_Trap
+Shapes_Trd
+Shapes_Tube
+Shapes_Tubs
+Shapes_TwistedTrap
+Shapes_UnidentifiedShape
+Shapes_Union
+Surface_Annulus
+Surface_Diamond
+Surface_Rectangle
+Surface_Trapezoid
+Transforms
+VirtualSurface
+dbversion
+)"};
+  EXPECT_EQ(output, expectedResponse);
+  //example: table 'Shapes' exists
+  EXPECT_TRUE(dbManager->checkTableFromDB("Shapes"));
+}
 
